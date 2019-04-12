39941e7122df216323f5b02a55c52fc3c88dd4c8
visualstudio <visualstudio@122-PC37>
2019-04-12 18:55:40 -0400

    working code commit

--- Assignment5_section2/Program.cs
+++ Assignment5_section2/Program.cs
@@ -10,7 +10,32 @@ namespace Assignment5_section2
     {
         static void Main(string[] args)
         {
-        }
 
+            Program a = new Program();
+            
+            Console.WriteLine(a.MyfunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical engine"));
+        }
+        public string MyfunctionA(string input)
+        {
+            string[] words = input.Split(' ');
+            int wordArraylength = words.Length;
+            int[] wordsLength = new int[wordArraylength];
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                wordsLength[i] = words[i].Length;
+            }
+
+            string currentWord = words[0];
+            string nextWord;
+
+            for(int y = 0; y < words.Length - 1; y++)
+            {
+                currentWord = words[y];
+                nextWord = words[y + 1];
+            }
+            return currentWord;
+        }
+        
     }
 }
